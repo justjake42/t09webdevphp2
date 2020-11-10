1267aa4f81375567c4ed42f66faa12426f58f1b8
breechoracion <breech@rmcordoviz.com>
2020-11-10 19:02:49 +0800

    fix: fixed transaction operation

--- insertTransactionData.php
+++ insertTransactionData.php
@@ -3,56 +3,56 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Insert Transaction Data</title>
 </head>
 <body>
     <form action="transactionEntryCon.php" method="post">
         <div class="col-2">
             <div class="input-group">
-                <label class="label">Name</label>
-                <input class="input--style-4" type="text" name="txtTRNo">
+                <label class="label">Transaction Number:</label>
+                <input class="input--style-4" type="number" name="txtTRNo">
             </div>
         </div>
         <div class="col-2">
             <div class="input-group">
-                <label class="label">Name</label>
+                <label class="label">Transaction Date:</label>
                 <input class="input--style-4" type="date" name="txtTDate">
             </div>
         </div>
         <div class="col-2">
             <div class="input-group">
-                <label class="label">Name</label>
-                <input class="input--style-4" type="text" name="txtSubTotal">
+                <label class="label">Subtotal:</label>
+                <input class="input--style-4" type="number" name="txtSubTotal">
             </div>
         </div>
         <div class="col-2">
             <div class="input-group">
-                <label class="label">Name</label>
-                <input class="input--style-4" type="text" name="txtProFee">
+                <label class="label">Processing Fee:</label>
+                <input class="input--style-4" type="number" name="txtProFee">
             </div>
         </div>
         <div class="col-2">
             <div class="input-group">
-                <label class="label">Name</label>
-                <input class="input--style-4" type="text" name="txteVAT">
+                <label class="label">VAT</label>
+                <input class="input--style-4" type="number" name="txteVAT">
             </div>
         </div>
         <div class="col-2">
             <div class="input-group">
-                <label class="label">Name</label>
-                <input class="input--style-4" type="text" name="txtNetCost">
+                <label class="label">Net Cost</label>
+                <input class="input--style-4" type="number" name="txtNetCost">
             </div>
         </div>
         <div class="col-2">
             <div class="input-group">
-                <label class="label">Name</label>
-                <input class="input--style-4" type="text" name="txtCENo">
+                <label class="label">Car Engine Number:</label>
+                <input class="input--style-4" type="number" name="txtCENo">
             </div>
         </div>
         <div class="col-2">
             <div class="input-group">
-                <label class="label">Name</label>
-                <input class="input--style-4" type="text" name="txtCLID">
+                <label class="label">Customer ID:</label>
+                <input class="input--style-4" type="number" name="txtCLID">
             </div>
         </div>
         <div class="p-t-15">
